e49d41fb4813692e41ef6238d4b4fd92a71521fc
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-04-05 22:16:10 +0600

    added introduction message and it's functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import React from "react";
 import LogoIcon from "../assets/images/chef-icon.png";
 import LightIcon from "../assets/images/light_mode_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 import DarkIcon from "../assets/images/dark_mode_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
-import React from "react";
+import HelpLogo from "../assets/images/help_24dp_FAFAF8_FILL0_wght400_GRAD0_opsz24.svg";
+import CrossIcon from "../assets/images/close_32dp_D17557_FILL0_wght400_GRAD0_opsz40.png";
 
 export default function Header() {
+  const [popup, setPopup] = React.useState(true);
+
   const systemPrefersDark = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
@@ -32,6 +36,18 @@ export default function Header() {
   }, [darkMode]);
   return (
     <header className="header">
+      {popup ? (
+        <div className="overlay" onClick={() => setPopup(false)}>
+          <p className="description">
+            This is a recipe generation app. You can get a recipe with the
+            ingredients you have. Just add them and click "Get a recipe". You
+            will need at least four ingredients to generate recipe. Enjoy!
+            <button className="popup-close-btn" onClick={() => setPopup(false)}>
+              <img src={CrossIcon} alt="close" />
+            </button>
+          </p>
+        </div>
+      ) : null}
       <img className="icon" src={LogoIcon} alt="Chef icon" />
       <h1 className="icon-title">Ai Chef</h1>
       <button
@@ -42,6 +58,9 @@ export default function Header() {
           src={darkMode ? LightIcon : DarkIcon}
           alt={darkMode ? "Light Mode" : "Dark Mode"}
         />
+      </button>
+      <button className="help" onClick={() => setPopup((prev) => !prev)}>
+        <img src={HelpLogo} alt="help" />
       </button>
     </header>
   );
