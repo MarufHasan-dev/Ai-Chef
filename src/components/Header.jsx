42c02f6180b8a6081b6dc5156fac47f1f51c8e7c
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-04-06 12:18:53 +0600

    polished the help dialogue box overlay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,11 +37,11 @@ export default function Header() {
   return (
     <header className="header">
       {popup ? (
-        <div className="overlay" onClick={() => setPopup(false)}>
+        <div className="overlay">
           <p className="description">
             This is a recipe generation app. You can get a recipe with the
-            ingredients you have. Just add them and click "Get a recipe". You
-            will need at least four ingredients to generate recipe. Enjoy!
+            ingredients you have. Just add them and click "Get a recipe." You
+            will need at least four ingredients to generate a recipe. Enjoy!
             <button className="popup-close-btn" onClick={() => setPopup(false)}>
               <img src={CrossIcon} alt="close" />
             </button>
